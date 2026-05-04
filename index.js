9395050199f2399509cdb581983749046a473bc9
Daniel <danielhiracheta909@gmail.com>
2026-05-03 22:47:03 -0600

    Agregada inserción de datos

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 async function probarConexion() {
     console.log("Conectando...")
     // Solo deja esta línea, borra las que tengan errores o digan 'paises'
-    const { data, error } = await supabase.from('estudiantes').select('*')
+    // --- BLOQUE PARA INSERTAR DATOS ---
+  const { data: insertData, error: insertError } = await supabase
+    .from('estudiantes')
+    .insert([
+      { nombre: 'Estudiante desde Código', carrera: 'FIME' }
+    ])
+    .select() // Esto es para que nos devuelva el dato que acaba de crear
 
+  if (insertError) {
+    console.log("Error al insertar:", insertError.message)
+  } else {
+    console.log("¡Dato insertado correctamente!", insertData)
+  }
+  // ----------------------------------
+    const { data, error } = await supabase.from('estudiantes').select('*')
     if (error) {
         console.log("Error de conexión:", error.message)
     } else {
